d5cc6c63c1664679f0588027f4f787d923a768a2
aNg310O <angelo.deprosperis@gmail.com>
2020-11-22 22:16:26 +0100

    modificata redirect a /home

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -7,7 +7,7 @@ export default class Profile extends Component {
     super(props);
 
     this.state = {
-      redirect: null,
+      redirect: "/home",
       userReady: false,
       currentUser: { username: "" }
     };
