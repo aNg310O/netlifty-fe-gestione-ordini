621cec72ddde8cb584b81ff38fa2853ed9007b29
aNg310O <angelo.deprosperis@gmail.com>
2020-11-22 22:02:41 +0100

    puntato heroku da auth service e user service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/auth/";
+const API_URL = "https://heroku-be-gestione-ordini.herokuapp.com/api/auth/";
 
 class AuthService {
   login(username, password) {
