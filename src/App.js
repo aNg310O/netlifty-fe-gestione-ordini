d888f9ee5774ca97f0b7e3e74b4b059a81fe929c
aNg310O <angelo.deprosperis@gmail.com>
2020-11-22 22:41:30 +0100

    modificato redirect logout a /home

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
                 </Link>
               </li>
               <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={this.logOut}>
+                <a href="/home" className="nav-link" onClick={this.logOut}>
                   LogOut
                 </a>
               </li>
