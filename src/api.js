54af171f19a971776f157a4e7db3278d3f527c3d
aNg310O <angelo.deprosperis@gmail.com>
2020-11-22 21:21:50 +0100

    modificata base url in api.js per puntare al be heroku

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   //baseURL: `http://localhost:8082/api/gestione-ordini/`,
-  baseURL: `http://localhost:8081/`,
+  //baseURL: `http://localhost:8081/`,
+  baseURL: `https://heroku-be-gestione-ordini.herokuapp.com/`,
   responseType: "json"
 });
