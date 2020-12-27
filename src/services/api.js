2ed63055735d528f8849eee1f3b27b03fe8dc5da
aNg310O <angelo.deprosperis@gmail.com>
2020-12-27 11:27:52 +0100

    modified heroku url

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   //baseURL: `http://localhost:8082/api/gestione-ordini/`,
   //baseURL: `http://localhost:8081/`,
-  baseURL: `https://heroku-be-gestione-ordini.herokuapp.com/`,
+  baseURL: `https://c1-gestione-ordini.herokuapp.com/`,
   responseType: "json"
 });
