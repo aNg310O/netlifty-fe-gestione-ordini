fb4d48237c19fe932e9f8ae26ec77746970c0490
aNg310O <angelo.deprosperis@gmail.com>
2021-01-06 00:10:42 +0100

    Add create admin user from admin page

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -22,11 +22,12 @@ class AuthService {
     localStorage.removeItem("user");
   }
 
-  register(username, email, password) {
+  register(username, email, password, roles) {
     return axios.post(API_URL + "signup", {
       username,
       email,
-      password
+      password,
+      roles
     });
   }
 
